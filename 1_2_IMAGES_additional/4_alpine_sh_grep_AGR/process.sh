91637ea45b3c38edc1b4f3bf64a67c9046f258b1
elya <vavina.elvira@gmail.com>
2025-10-28 01:31:44 +0300

    fix some in 4_alpine: ARG -> ENV, sed arg

--- 1_2_IMAGES_additional/4_alpine_sh_grep_AGR/process.sh
+++ 1_2_IMAGES_additional/4_alpine_sh_grep_AGR/process.sh
@@ -5,6 +5,12 @@ echo "Running with $TEXT_TOOL on $1"
 if [ -z "$TEXT_TOOL" ]; then
   echo "No TEXT_TOOL set, defaulting to grep"
   TEXT_TOOL="grep"
+
+elif [ "$TEXT_TOOL" = "sed" ]; then
+
+  $TEXT_TOOL "s/ERROR/NO_ERROR/" $1
+  exit 0
+
 fi
 
 $TEXT_TOOL "ERROR" $1
